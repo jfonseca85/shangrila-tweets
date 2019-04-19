edfb798b8f7ad716c87a32c3b5b2ac74c05a3031
jfonseca85 <luis.engcomp@gmail.com>
2019-04-19 20:21:32 -0300

    Adicionando dependecias do Cassandra

--- shangrila-consumer/src/main/java/com/twitter/consumer/service/SparkConsumerService.java
+++ shangrila-consumer/src/main/java/com/twitter/consumer/service/SparkConsumerService.java
@@ -6,6 +6,7 @@ import java.util.Date;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaInputDStream;
@@ -18,6 +19,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.datastax.driver.core.Session;
+import com.datastax.spark.connector.cql.CassandraConnector;
 import com.twitter.consumer.config.KafkaConsumerConfig;
 import com.twitter.consumer.config.KafkaProperties;
 import com.twitter.consumer.model.TweetPlayload;
@@ -51,53 +54,52 @@ public class SparkConsumerService {
 
 		// Create context with a 10 seconds batch interval
 		JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(10));
+		createConection(sparkConf);
 
 		// Create direct kafka stream with brokers and topics
 		JavaInputDStream<ConsumerRecord<String, String>> messages = KafkaUtils.createDirectStream(jssc,
 				LocationStrategies.PreferConsistent(),
 				ConsumerStrategies.Subscribe(topics, kafkaConsumerConfig.consumerConfigs()));
 
-		JavaPairDStream<String, String> pairRDD = messages.mapToPair(record -> new Tuple2<>(record.key(), record.value()));
-
 //        //Coleta de seguidores por Usuarios para as amostras
 		JavaDStream<String> lines = messages.map(stringStringConsumerRecord -> stringStringConsumerRecord.value());
 		log.info("<<<<<<<<<<<<< INICIO - Salvando do playLoad no Banco >>>>>>>>>>>>>>>>>>>>>");
-	    lines
-	    	 .flatMap(text -> HashTagsUtils.hashTagsFromTweetAmostras(text))
-	         .foreachRDD(rrdd -> {
-	             //Counts
-	             rrdd.collect()
-	                     .forEach(record -> {
-	                    	 TweetPlayload tweetPlayload = new TweetPlayload();
-	                    	 tweetPlayload = TweetPlayload.builder(record);
-	                    	 log.info("Salvando do playLoad no Banco >>>> : {} ", tweetPlayload.toString());
-	             });
-	         });
-	    log.info("<<<<<<<<<<<<< FIM - Salvando do playLoad no Banco >>>>>>>>>>>>>>>>>>>>>");
-	    
-	    
-        //Coleta de seguidores por Usuarios para as amostras
-	    log.info("<<<<<<<<<<<<< INICIO - Salvando do playLoad Tweets por Usuario, hashTag and Idioma >>>>>>>>>>>>>>>>>>>>>");
-	    lines
-	    	 .flatMap(hashTagsByUserAndLanguage -> HashTagsUtils.hashTagsByUserAndLanguage(hashTagsByUserAndLanguage)) // Listar os Tweets por usuario 
-             .mapToPair(hashTag -> new Tuple2<>(hashTag, 1))
-             .reduceByKey((a, b) -> Integer.sum(a, b))
-             .mapToPair(stringIntegerTuple2 -> stringIntegerTuple2.swap())
-                .foreachRDD(rrdd -> {
-                    //Counts
-                    rrdd.sortByKey(false).collect()
-                            .forEach(record -> {
-                                log.info("Agrupando pelo usuario: <<<<<<<:>>>>>>>>> "+String.format(" %s (%d)", record._2, record._1));
-                    });
-                });
-	    log.info("<<<<<<<<<<<<< FIM - Salvando do playLoad Tweets por Usuario, hashTag and Idioma >>>>>>>>>>>>>>>>>>>>>");    
+		lines.flatMap(text -> HashTagsUtils.hashTagsFromTweetAmostras(text)).foreachRDD(rrdd -> {
+			// Counts
+			rrdd.collect().forEach(record -> {
+				TweetPlayload tweetPlayload = new TweetPlayload();
+				tweetPlayload = TweetPlayload.builder(record);
+				log.info("Salvando do playLoad no Banco >>>> : {} ", tweetPlayload.toString());
+			});
+		});
+		log.info("<<<<<<<<<<<<< FIM - Salvando do playLoad no Banco >>>>>>>>>>>>>>>>>>>>>");
+
+		// Coleta de seguidores por Usuarios para as amostras
+		log.info(
+				"<<<<<<<<<<<<< INICIO - Salvando do playLoad Tweets por Usuario, hashTag and Idioma >>>>>>>>>>>>>>>>>>>>>");
+		lines.flatMap(hashTagsByUserAndLanguage -> HashTagsUtils.hashTagsByUserAndLanguage(hashTagsByUserAndLanguage)) // Listar
+																														// os
+																														// Tweets
+																														// por
+																														// usuario
+				.mapToPair(hashTag -> new Tuple2<>(hashTag, 1)).reduceByKey((a, b) -> Integer.sum(a, b))
+				.mapToPair(stringIntegerTuple2 -> stringIntegerTuple2.swap()).foreachRDD(rrdd -> {
+					// Counts
+					rrdd.sortByKey(false).collect().forEach(record -> {
+						log.info("Agrupando pelo usuario: <<<<<<<:>>>>>>>>> "
+								+ String.format(" %s (%d)", record._2, record._1));
+					});
+				});
+		log.info(
+				"<<<<<<<<<<<<< FIM - Salvando do playLoad Tweets por Usuario, hashTag and Idioma >>>>>>>>>>>>>>>>>>>>>");
 
 		log.info("<<<<<<<<<<<<< INICIO - Agrupando por Hora >>>>>>>>>>>>>>>>>>>>>");
 		JavaDStream<String> wordCounts = lines.flatMap(text -> HashTagsUtils.hashTagsFromTweet(text));
 		wordCounts.print();
 		wordCounts.window(Durations.seconds(60), Durations.seconds(10)).countByValue()
-				.foreachRDD(tRDD -> tRDD.foreach(x -> 
-				      System.out.println(new Date() + " ::The window count  60 segundos, remove depois de 5 segundos tag is ::" + x._1() + " and the val is ::" + x._2())));
+				.foreachRDD(tRDD -> tRDD.foreach(x -> System.out
+						.println(new Date() + " ::The window count  60 segundos, remove depois de 5 segundos tag is ::"
+								+ x._1() + " and the val is ::" + x._2())));
 		log.info("<<<<<<<<<<<<< FIM - Agrupando por Horas >>>>>>>>>>>>>>>>>>>>>");
 
 		// Start the computation
@@ -109,4 +111,19 @@ public class SparkConsumerService {
 			// Restore interrupted state...
 		}
 	}
+
+	private void createConection() {
+		CassandraConnector connector = CassandraConnector.apply(sparkConf);
+
+		// Prepare the schema
+		try (Session session = connector.openSession()) {
+			session.execute("DROP KEYSPACE IF EXISTS java_api");
+			session.execute("CREATE KEYSPACE java_api WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}");
+			session.execute("CREATE TABLE java_api.products (id INT PRIMARY KEY, name TEXT, parents LIST<INT>)");
+			session.execute("CREATE TABLE java_api.sales (id UUID PRIMARY KEY, product INT, price DECIMAL)");
+			session.execute("CREATE TABLE java_api.summaries (product INT PRIMARY KEY, summary DECIMAL)");
+		}
+
+	}
+
 }
