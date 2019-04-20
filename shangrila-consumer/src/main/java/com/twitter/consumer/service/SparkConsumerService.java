110e799171e9638d30ab2ff7c22662ced34259b8
jfonseca85 <luis.engcomp@gmail.com>
2019-04-19 22:54:43 -0300

    ultimas alteracoes

--- shangrila-consumer/src/main/java/com/twitter/consumer/service/SparkConsumerService.java
+++ shangrila-consumer/src/main/java/com/twitter/consumer/service/SparkConsumerService.java
@@ -1,16 +1,23 @@
 package com.twitter.consumer.service;
 
+import static com.datastax.spark.connector.japi.CassandraJavaUtil.javaFunctions;
+import static com.datastax.spark.connector.japi.CassandraJavaUtil.mapToRow;
+import com.datastax.spark.connector.japi.CassandraRow;
+
+import java.io.Serializable;
+import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaInputDStream;
-import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka010.ConsumerStrategies;
 import org.apache.spark.streaming.kafka010.KafkaUtils;
@@ -21,6 +28,7 @@ import org.springframework.stereotype.Service;
 
 import com.datastax.driver.core.Session;
 import com.datastax.spark.connector.cql.CassandraConnector;
+import com.datastax.spark.connector.japi.rdd.CassandraJavaRDD;
 import com.twitter.consumer.config.KafkaConsumerConfig;
 import com.twitter.consumer.config.KafkaProperties;
 import com.twitter.consumer.model.TweetPlayload;
@@ -33,7 +41,7 @@ public class SparkConsumerService {
 
 	private final Logger log = LoggerFactory.getLogger(SparkConsumerService.class);
 
-	private final SparkConf sparkConf;
+	private SparkConf sparkConf;
 
 	private final KafkaConsumerConfig kafkaConsumerConfig;
 
@@ -53,8 +61,47 @@ public class SparkConsumerService {
 		log.debug("Running Spark Consumer Service..");
 
 		// Create context with a 10 seconds batch interval
-		JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(10));
-		createConection(sparkConf);
+		
+//		sparkConf = new SparkConf(true)
+//                .setAppName("TwitterToCassandra")
+//                .setMaster("local[*]")
+//                .set("spark.cassandra.connection.host", "192.168.99.100")
+//                .set("spark.cassandra.connection.port", "9042")
+//                .set("spark.driver.allowMultipleContexts", "true")
+//				.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+//				.set("spark.cassandra.output.batch.size.bytes", "2M");
+		
+		JavaSparkContext sc = new JavaSparkContext(sparkConf);
+		JavaStreamingContext jssc = new JavaStreamingContext(sc, Durations.seconds(10));
+		
+
+//		CassandraConnector connector = CassandraConnector.apply(sparkConf);
+//		try (Session session = connector.openSession()) {
+//			session.execute("DROP KEYSPACE IF EXISTS simple_canary_cc");
+//			session.execute("CREATE KEYSPACE simple_canary_cc WITH "
+//					+ "REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1}");
+//			session.execute("CREATE TABLE simple_canary_cc.simple_pair " + "(key int PRIMARY KEY, value text)");
+//		}
+
+//		List<SimplePair> pairs = Arrays.asList(new SimplePair(1, "One"), new SimplePair(5, "Five"),
+//				new SimplePair(6, "Six"), new SimplePair(7, "Seven"), new SimplePair(9, "Nine"),
+//				new SimplePair(10, "Ten"), new SimplePair(12, "Twelve"), new SimplePair(16, "Sixteen"),
+//				new SimplePair(19, "Nineteen"));
+//
+//		JavaRDD<SimplePair> simplekvRDD = sc.parallelize(pairs);
+//		javaFunctions(simplekvRDD).writerBuilder("simple_canary_cc", "simple_pair", mapToRow(SimplePair.class))
+//				.saveToCassandra();
+//
+//		CassandraJavaRDD<com.datastax.spark.connector.japi.CassandraRow> rdd = javaFunctions(sc).cassandraTable("simple_canary_cc", "simple_pair")
+//				.select("key", "value");
+//
+//		long count = rdd.count();
+//		System.out.format("Count: %d %n", count);
+//
+//		List somePairs = rdd.take(9);
+//		System.out.println(somePairs);
+		
+		
 
 		// Create direct kafka stream with brokers and topics
 		JavaInputDStream<ConsumerRecord<String, String>> messages = KafkaUtils.createDirectStream(jssc,
@@ -77,11 +124,7 @@ public class SparkConsumerService {
 		// Coleta de seguidores por Usuarios para as amostras
 		log.info(
 				"<<<<<<<<<<<<< INICIO - Salvando do playLoad Tweets por Usuario, hashTag and Idioma >>>>>>>>>>>>>>>>>>>>>");
-		lines.flatMap(hashTagsByUserAndLanguage -> HashTagsUtils.hashTagsByUserAndLanguage(hashTagsByUserAndLanguage)) // Listar
-																														// os
-																														// Tweets
-																														// por
-																														// usuario
+		lines.flatMap(hashTagsByUserAndLanguage -> HashTagsUtils.hashTagsByUserAndLanguage(hashTagsByUserAndLanguage)) 
 				.mapToPair(hashTag -> new Tuple2<>(hashTag, 1)).reduceByKey((a, b) -> Integer.sum(a, b))
 				.mapToPair(stringIntegerTuple2 -> stringIntegerTuple2.swap()).foreachRDD(rrdd -> {
 					// Counts
@@ -94,7 +137,7 @@ public class SparkConsumerService {
 				"<<<<<<<<<<<<< FIM - Salvando do playLoad Tweets por Usuario, hashTag and Idioma >>>>>>>>>>>>>>>>>>>>>");
 
 		log.info("<<<<<<<<<<<<< INICIO - Agrupando por Hora >>>>>>>>>>>>>>>>>>>>>");
-		JavaDStream<String> wordCounts = lines.flatMap(text -> HashTagsUtils.hashTagsFromTweet(text));
+		JavaDStream<String> wordCounts = lines.flatMap(text -> HashTagsUtils.anyashTagsFromTweet(text));
 		wordCounts.print();
 		wordCounts.window(Durations.seconds(60), Durations.seconds(10)).countByValue()
 				.foreachRDD(tRDD -> tRDD.foreach(x -> System.out
@@ -110,20 +153,6 @@ public class SparkConsumerService {
 			log.error("Interrupted: {}", e);
 			// Restore interrupted state...
 		}
-	}
-
-	private void createConection() {
-		CassandraConnector connector = CassandraConnector.apply(sparkConf);
-
-		// Prepare the schema
-		try (Session session = connector.openSession()) {
-			session.execute("DROP KEYSPACE IF EXISTS java_api");
-			session.execute("CREATE KEYSPACE java_api WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}");
-			session.execute("CREATE TABLE java_api.products (id INT PRIMARY KEY, name TEXT, parents LIST<INT>)");
-			session.execute("CREATE TABLE java_api.sales (id UUID PRIMARY KEY, product INT, price DECIMAL)");
-			session.execute("CREATE TABLE java_api.summaries (product INT PRIMARY KEY, summary DECIMAL)");
-		}
-
 	}
 
 }
